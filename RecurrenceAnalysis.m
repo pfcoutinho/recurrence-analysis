e9944c10192890f6048765a3769da7e33b6e99ad
patrick <pfcoutinho@outlook.com>
2020-01-28 18:12:01 -0300

    Creates DistanceMatrix, RecurrencePlot, and RecurrenceAnalysis classes

---
+++ RecurrenceAnalysis.m
@@ -0,0 +1,60 @@
+classdef RecurrenceAnalysis
+%RECURRENCEANALYSIS Recurrence analysis of recurrence plots
+%   This class performs recurrence quantification analysis (RQA) of recurrence 
+%   plots, which are binary matrices (i.e., A(i, j) = {0, 1}).
+%
+% CONTACT
+%   Patrick Franco Coutinho
+%   pfcoutinho@outlook.com
+%
+% Last update: Jan 28, 2020
+% ============================================================================ %
+    
+    properties
+        diagonalHistogram   % Histogram of diagonal lines
+        verticalHistogram   % Histogram of vertical lines
+    end
+    
+    properties (Dependent)
+        RR                  % Recurrence rate (RR)
+        DET                 % Determinism (DET)
+        ENTD                % Entropy of diagonal lines (ENTD)
+        ENTV                % Entropy of vertical lines (ENTV)
+    end
+    
+    methods
+        function obj = RecurrenceAnalysis()
+            
+        end
+        
+        function recurrencerate()
+            
+        end
+        
+        function getrecurrencerate()
+            
+        end
+        
+        function determinism()
+            
+        end
+        
+        function getdeterminism()
+        end
+        
+        function entropyd()
+        end
+        
+        function getentropyd()
+        end
+        
+        function entropyv()
+        end
+        
+        function getentropyv()
+        end
+        
+        function adaptivethreshold(rate)
+        end
+    end
+end
