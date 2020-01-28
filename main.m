7003d0e1f27868d0a4929590b1861907bf2aff87
patrick <pfcoutinho@outlook.com>
2020-01-28 18:13:18 -0300

    Changes main.m

--- main.m
+++ main.m
@@ -1,95 +1,117 @@
+% main.m
 %
-% The purpose of this file is to test the following functions:
-%   distmatrix.m
-%   recplot.m
-%   plotrp.m
+% The purpose of this file is to test the following classes:
+%   DistanceMatrix
+%   RecurrencePlot
+%   RecurrenceAnalysis
 %
-
+% CONTACT
+%   Patrick Franco Coutinho
+%   pfcoutinho@outlook.com
+%
+% Last update: Jan 28, 2020
 %% Distance matrix and recurrence plot for a sinusoidal time series
 close all
 clear
 clc
 
-x = sin(2*pi*0.05*(1:100));
+data = sin(2*pi*0.05*(1:100));
 
-RP = recplot(2, 3, 0.2, 'L1', x);
+RP = RecurrencePlot(2, 3, 0.2, 'L2', data);
+D  = DistanceMatrix(2, 3, 'L2', data);
 
 figure()
 
 subplot(5, 1, 1)
-plot(x)
+plot(data)
 ylabel("x(t)")
 xlabel("t")
 xlim([1, 100])
 set(gca, "XTick", [1, 10:10:100])
 
 subplot(5, 1, [2, 5])
-plotrp(RP)
+RP.plot()
+
+figure()
+
+subplot(5, 1, 1)
+plot(data)
+ylabel("x(t)")
+xlabel("t")
+xlim([1, 100])
+set(gca, "XTick", [1, 10:10:100])
+
+subplot(5, 1, [2, 5])
+D.plot()
 
 %% Distance matrix and recurrence plot for a random time series
 close all
 clear
 clc
 
-x = rand(100, 1);
+data = rand(100, 1);
 
-RP = recplot(1, 1, 0.2, 'L1', x);
+RP = RecurrencePlot(1, 1, 0.2, 'L1', data);
 
 figure()
 
 subplot(5, 1, 1)
-plot(x)
+plot(data)
 ylabel("x(t)")
 xlabel("t")
 xlim([1, 100])
 set(gca, "XTick", [1, 10:10:100])
 
 subplot(5, 1, [2, 5])
-plotrp(RP)
+RP.plot()
 
 %% Corridor thresholded version of the recurrence plot for one time series
 close all
 clear
 clc
 
-x = rand(100, 1);
+data = rand(100, 1);
 
-RP = recplot(1, 1, [0.1, 0.2], 'Linf', x);
+RP = RecurrencePlot(1, 1, [0.1, 0.2], 'L-infinity', data);
 
 figure()
 
 subplot(5, 1, 1)
-plot(x)
+plot(data)
 ylabel("x(t)")
 xlabel("t")
 xlim([1, 100])
 set(gca, "XTick", [1, 10:10:100])
 
 subplot(5, 1, [2, 5])
-plotrp(RP)
+RP.plot()
 
 %% Cross-recurrence plot between two time series
+%{
 close all
 clear
 clc
 
-x = rand(100, 1);
-y = rand(150, 1);
+data1 = rand(100, 1);
+data2 = rand(150, 1);
 
-XRP = recplot(1, 1, 0.2, 'Linf', x, y);
+XRP = (1, 1, 0.2, 'L-infinity', data1, data2);
 
 figure()
 plotrp(XRP)
+%}
 
 %% Corridor thresholded version of cross-recurrence plot between two time series
+%{
 close all
 clear
 clc
 
-x = rand(100, 1);
-y = rand(150, 1);
+data1 = rand(100, 1);
+data2 = randn(150, 1);
 
-XRP = recplot(1, 1, [0.1, 0.2], 'Linf', x, y);
+XRP = recplot(1, 1, [0.1, 0.2], 'Linf', data1, data2);
 
 figure()
 plotrp(XRP)
+%}
