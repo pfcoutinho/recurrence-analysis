c51a734b337785d94281efb8345d9b04290bf787
patrick <pfcoutinho@outlook.com>
2020-05-12 18:06:21 -0300

    Creates a new event handler to deal with changes of the threshold

--- RecurrencePlot.m
+++ RecurrencePlot.m
@@ -1,6 +1,12 @@
 classdef RecurrencePlot < DistanceMatrix & handle
-%RECURRENCEPLOT Recurrence Plot
-%   
+%RECURRENCEPLOT Recurrence plot class
+%   Recurrence plot (RP) is a tool for non-linear data analysis. It is a binary
+%   matrix in which the recurrence points are represented by black or colored
+%   points. RPs are a kind of thresholded similarity matrix and reveal interes-
+%   ting recurrence patterns of dynamical systems. RP is also knwon as recurren-
+%   ce matrix.
+%   This class can be used to generate an RP from a single data series or the
+%   cross-recurrence plot (CRP) between two data series.
 %
 % DEPENDECIES
 %   DistanceMatrix.m
@@ -17,20 +23,30 @@ classdef RecurrencePlot < DistanceMatrix & handle
 %   threshold
 %   normType
 %
+% REFERENCES
+%   
+%
 % AUTHOR
 %   Patrick Franco Coutinho
 %   pfcoutinho@outlook.com
 %
-% Last update: Mar 10, 2020
+% Last update: May 11, 2020
 % ============================================================================ %
     %
     % Properties
     %
     properties
-        % Recurrence parameters
-        % Threshold
-        threshold double {mustBeNumeric, mustBeReal, mustBeNonnegative}
-    end % END properties
+        threshold           % threshold
+        
+        RP = []             % recurrence plot / cross recurrence plot
+    end %END properties
+    
+    %
+    % Events
+    %
+    events
+        thresholdChangeEvt
+    end
     
     %
     % Methods
@@ -44,23 +60,27 @@ classdef RecurrencePlot < DistanceMatrix & handle
             self = self@DistanceMatrix(embeddingDimension, timeDelay, ...
                         normType, varargin{1:end});
             
-            % Recurrence parameters
+            % set threshold
             self.threshold = threshold;
-                        
-            % Recurrence matrix
-            self.M = rp(self);
-        end % END RecurrencePlot() 
+            
+            % calculates the recurrence matrix
+            self.RP = rp(self);
+
+            % add listeners to the event that is triggered when threshold is
+            % changed
+            addlistener(self, 'thresholdChangeEvt', @thresholdevthandler);
+        end %END RecurrencePlot() 
         
 
         function plot(self, varargin)
         %PLOT Plot recurrence plot
         %   Plot the recurrence or cross-recurrence plot
         % -------------------------------------------------------------------- %
-            imagesc(self.M)
-
+            imagesc(self.RP);
+            
             if(nargin == 2)
                 if(strcmp(varargin{1}, 'color'))
-                    colormap([1, 1, 1; rand(1, 3).*[1, 0.7, 1]])
+                    colormap([1, 1, 1; rand(1, 3).*[0.9, 0.7, 0.95]])
                 end
             else
                 colormap([1, 1, 1; 0, 0, 0])
@@ -77,79 +97,112 @@ classdef RecurrencePlot < DistanceMatrix & handle
             ay = [1, round(m/2), m];
             set(gca, 'XTick', ax)
             set(gca, 'YTick', ay)
-        end % END plot()
+        end %END plot()
         
 
         function set.threshold(self, value)
-        %SET.THRESHOLD Set the value of the threshold
-        %   Validation: threshold can be any real number greater than or equal 
-        %   to 0. Input can be of the form of single scalar (e.g., 0.5) or of
-        %   an interval (e.g., [0.1, 0.2]).
-        % -------------------------------------------------------------------- %            
-            DEFAULT_ERR_MSG = "Invalid parameter: threshold.";
+        %SET.THRESHOLD Threshold
+        %
+        %   VALIDATION Threshold must be:
+        %       * non-empty,
+        %       * real valued,
+        %       * greater than or equal to 0, and
+        %       * a scalar or an interval (e.g., [0.2, 0.4]).
+        % -------------------------------------------------------------------- %
+            value = self.chkthreshold(value);
             
-            % Check if value is empty
-            if(isempty(value))
-                ERR_MSG = strcat(DEFAULT_ERR_MSG, " Value cannot be empty.");
-                error(ERR_MSG);
-            end
-            
-            % Check size of value
-            [m, n] = size(value);
-                
-            if(m < 1 || m > 2 || n < 1 || n > 2 || (m == 2 && n == 2))
-                ERR_MSG = strcat(DEFAULT_ERR_MSG, " It must be a scalar ", ....
-                    "or a vector with exactly two elements.");
-                error(ERR_MSG);
-            end
-            
-            % Check if threshold is already set
-            if(isempty(self.threshold))
-                flag = false;
-            else
-                flag = true;
-            end
-
             % Set threshold value and notify listeners if value changes
-            if(~flag || self.threshold ~= value)
+            if(isempty(self.threshold) || self.threshold ~= value)
                 self.threshold = value;
-                
-                if(flag)
-                    notify(self, 'parameterChangeEvt');
-                end
+                notify(self, 'thresholdChangeEvt');
             end
-        end % END set.threshold()
+        end %END set.threshold()
         
-    end % END public methods
+    end %END public methods
     
     
     methods (Access = protected)
         
-        function self = evthandle(self, ~)
+        function self = evthandler(self, ~)
         % -------------------------------------------------------------------- %
-            self = evthandle@DistanceMatrix(self);
-            self.M = rp(self);
+            self = evthandler@DistanceMatrix(self);            
+            if ~isempty(self.RP)
+                self.RP = rp(self);
+            end
         end
         
-    end % END protected methods
-    
-    
-    methods (Access = private)
+        function self = thresholdevthandler(self, ~)
+        % -------------------------------------------------------------------- %
+            if ~isempty(self.RP)
+                self.RP = rp(self);
+            end
+        end
         
-        function M = rp(self)
+        
+        function RP = rp(self)
+        %RP Recurrence plot
+        %   This function applies the threshold (or the corridor threshold) to
+        %   the distance matrix.
         % -------------------------------------------------------------------- %
             [m, n] = size(self.threshold);
             
             if(m == 1 && n == 1)
-                % Recurrence plot
-                M = (self.M <= self.threshold);
+                % recurrence plot / cross-recurrence plot
+                RP = (self.M <= self.threshold);
             else
-                % Corridor recurrence plot
-                M = and(self.M >= self.threshold(1), ...
+                % corridor recurrence plot / cross recurrence plot
+                RP = and(self.M >= self.threshold(1), ...
                         self.M <= self.threshold(2));
             end 
-        end % END rp()
+        end %END rp()
         
-    end % END private methods
+        
+        function value = chkthreshold(self, value)
+        %CHKTHRESHOLD Threshold
+        %   Validate threshold value before assigning it to a RecurrencePlot
+        %   object.
+        % -------------------------------------------------------------------- %
+            parameterName = "threshold";
+            self.verifyifempty(value, parameterName);
+            self.verifyifnumeric(value, parameterName);
+            self.verifyifreal(value, parameterName);
+
+            % check if value is a scalar or an array; also, if it is an array,
+            % check its size and sort the elements
+            if ~isscalar(value)
+                if ~isvector(value)
+                    ERR_MSG = strcat(DEFAULT_ERR_MSG, " It must be a ", ...
+                    "scalar or a vector with exactly two elements.");
+                    error(ERR_MSG);
+                else
+                    % check vector sizes
+                    [m, ~] = size(value);
+                    if m == 2
+                        value = value';
+                    end
+                    
+                    % sort elements in ascending order
+                    value = sort(value, 'ascend');
+                end
+            end
+        end %END chkthreshold()
+        
+    end %END protected methods
     
+    
+    methods (Access = protected, Static = true)
+        
+        function verifyifempty(value, parameterName)
+            verifyifempty@DistanceMatrix(value, parameterName);
+        end %END verifyifempty()
+        
+        function verifyifnumeric(value, parameterName)
+            verifyifnumeric@DistanceMatrix(value, parameterName);
+        end %END verifyifnumeric()
+        
+        function verifyifreal(value, parameterName)
+            verifyifreal@DistanceMatrix(value, parameterName);
+        end %END verifyifreal()
+        
+    end %END protected, static methods
 end
